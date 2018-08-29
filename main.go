17a5c05edf19cbae1a0ce3f488fc2c6d45b70f24
Hrant Mirijanyan <harvey.mirijanyan@gmail.com>
2018-08-28 18:06:10 -0700

    Remove godotenv

--- main.go
+++ main.go
@@ -3,17 +3,10 @@ package main // import "github.com/harvey93/pipeline"
 import (
 	"fmt"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Println("Pipeline")
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Println("Error loading .env")
-	}
-
 	port := os.Getenv("PORT")
 	fmt.Println(port)
 
