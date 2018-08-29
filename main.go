b4354cae88262fcd15bff595d1b1b02f684e23ba
Hrant Mirijanyan <harvey.mirijanyan@gmail.com>
2018-08-29 08:24:31 -0700

    Add dev flag to command line args

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main // import "github.com/harvey93/pipeline"
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Println("Pipeline")
+	devP := flag.Bool("dev", false, "environment")
+	flag.Parse()
+	if *devP {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
 	port := os.Getenv("PORT")
 	fmt.Println(port)
 
